35ea330dee823d27b6e77311ea50b2a774713e8f
krrish yaduka <kyaduka2803@gmail.com>
2025-06-25 23:17:58 +0530

    Add files via upload

---
+++ rag.py
@@ -0,0 +1,47 @@
+from langchain_community.vectorstores import FAISS
+from groq import Groq
+import os
+from config import DATA
+from langchain_huggingface import HuggingFaceEmbeddings
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+def get_answer(query, role):
+    embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-base-en-v1.5")
+    
+    db = FAISS.load_local(f"{DATA}/{role}/faiss_index", embeddings=embeddings,allow_dangerous_deserialization=True)
+    role_key = role.lower()
+    if role == "C-Level":
+        docs = db.similarity_search(query, k=4)
+    elif role == "Employee":
+        docs = db.similarity_search(query, k=4, filter={"role": "general"})
+    else:
+        docs = db.similarity_search(query, k=4, filter={"role": role_key})
+
+        # Build context from retrieved docs
+    context = "\n\n".join([doc.page_content for doc in docs])
+    sources = [doc.metadata.get("source", "unknown") for doc in docs]    
+
+        # Prompt
+    prompt = f"""
+You are a helpful assistant at FinSolve Technologies. Use the context below to answer the query.
+
+Query: {query}
+
+    Context:
+    {context}
+
+Answer:"""    
+
+    try:
+        response = client.chat.completions.create(model="llama3-70b-8192",
+                                                  messages=[
+                                                      {"role": "system", "content": "You are a helpful assistant."},
+                                                      {"role": "user", "content": prompt}
+                                                  ])
+        answer = response.choices[0].message.content.strip()
+    except Exception as e:
+        answer = "Error generating response"
+        sources = [str(e)] 
+    return answer, sources
+        
